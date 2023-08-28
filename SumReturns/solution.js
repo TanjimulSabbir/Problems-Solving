c6e7143fe7f74af6400471ce255a9690474b9c1e
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-28 17:47:56 +0600

    inital-commit

---
+++ SumReturns/solution.js
@@ -0,0 +1,17 @@
+// write a function that return the two numbers.
+// write a functin that return the sum of all numbers (regardless of # params)
+
+const TwoNumberSum = (params1, params2) => {
+    return params1 + params2
+}
+
+function SumAllNumbers(...Params) {
+    let sum = 0;
+    Params.forEach(element => {
+        sum += element;
+    })
+    return sum;
+}
+
+console.log(TwoNumberSum(1, 4), "TwoNumberSum");
+console.log(SumAllNumbers(1, 2, 3, 5, 6, 7, 8.5, 9), "AllNumerSum");
